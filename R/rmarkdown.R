724ed76c29d58386521ed462049c2c64285230bf
Bangyou Zheng <bangyou.zheng@csiro.au>
2022-06-13 15:22:45 +1000

    Fix the bug to find image name

--- R/rmarkdown.R
+++ R/rmarkdown.R
@@ -5,6 +5,7 @@
 #' @param host the host of tiddlywiki web server
 #' @param path The folder of tiddlywiki. Temp solution as no PUT file api in tiddlywiki WebServer.
 #' @param tags tiddler tags
+#' @param ... Other argument pass to md_document
 #' @return R Markdown output format to pass to render()
 #' @export
 #'
@@ -13,13 +14,16 @@
 #' library(rmarkdown)
 #' render("input.Rmd")
 #' }
-tiddler_document <- function(host = NULL, path = NULL, tags = NULL) {
+tiddler_document <- function(host = NULL, path = NULL, tags = NULL, ...) {
 
     # Get md document
-    output <- rmarkdown::md_document()
+    output <- rmarkdown::md_document(...)
     # Define post processor function
     post_processor <- function(metadata, input_file, output_file, clean, verbose) {
-        file.copy(input_file, paste0(input_file, '.bck'))
+        print(input_file)
+        print(output_file)
+        file.copy(input_file, paste0(input_file, ".bck"))
+        file.copy(output_file, paste0(output_file, ".bck"))
         title <- readLines(input_file)
         pattern <- "^title: *\"(.+)\"$ *"
         pos <- grepl(pattern = pattern, title)
@@ -30,41 +34,44 @@ tiddler_document <- function(host = NULL, path = NULL, tags = NULL) {
         }
         text <- readLines(output_file)
         # Update image file path
-        pattern <- "^(\\!\\[\\]\\()(.+\\.png)(\\))$"
+        pattern <- c("^(\\!\\[\\]\\()(.+\\.\\w{3})(\\))$",
+                     "^(<img +src=\")(.+\\.\\w{3})(\".*/>)$")
+        i <- 1
+        for (i in seq(along = pattern)) {
+            pos_i <- grep(pattern[i], text)
+            if (length(pos_i) > 0) {
+                img_files_raw <- gsub(pattern[i], "\\2", text[pos_i])
 
-        pos <- grepl(pattern, text)
-        if (sum(pos) > 0) {
-            img_files_raw <- gsub(pattern, "\\2", text[pos])
-            img_files_new <- img_files_raw
-            pos_img <- file.exists(img_files_new)
-            if (sum(!pos_img) > 0) {
-                stop("Image files do not exist: ",
-                     paste(img_files_new[!pos_img], collapse = ", "))
-            }
-            # Copy files to node.js server. Will update if there are PUT file API.
-            if (!is.null(path)) {
-                target_files <- file.path(path, "files",
-                                          img_files_raw)
-                dir_names <- unique(dirname(target_files))
-                for (i in seq(along = dir_names)) {
-                    if (!dir.exists(dir_names[i])) {
-                        dir.create(dir_names[i], recursive = TRUE)
-                    }
+                pos_img <- file.exists(img_files_raw)
+                if (sum(!pos_img) > 0) {
+                    stop("Image files do not exist: ",
+                         paste(img_files_raw[!pos_img], collapse = ", "))
                 }
+                # Copy files to node.js server. Will update if there are PUT file API.
+                if (!is.null(path)) {
+                    target_files <- file.path(path, "files",
+                                              img_files_raw)
+                    dir_names <- unique(dirname(target_files))
+                    for (j in seq(along = dir_names)) {
+                        if (!dir.exists(dir_names[j])) {
+                            dir.create(dir_names[j], recursive = TRUE)
+                        }
+                    }
 
-                file.copy(img_files_new,
-                          target_files,
-                          overwrite = TRUE)
+                    file.copy(img_files_raw,
+                              target_files,
+                              overwrite = TRUE)
+                }
+                text[pos_i] <- gsub(pattern[i], "\\1./files/\\2\\3", text[pos_i])
             }
-            text[pos] <- gsub(pattern, "\\1./files/\\2\\3", text[pos])
-
         }
         body <- .tiddler_json(title = title,
                               text = text,
                               type = "text/x-markdown",
                               tags = tags)
 
-        output_file <- file.path(dirname(output_file), paste0(title, ".json"))
+        output_file <- file.path(dirname(output_file),
+                                 paste0(tools::file_path_sans_ext(basename(output_file)), ".json"))
         writeLines(body, output_file)
         # Push into server if host is specified
         if (!is.null(host)) {
