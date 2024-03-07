03a9444d334b88208e4948569284b7fe2e04a303
Bangyou Zheng <zheng.bangyou@gmail.com>
2024-03-07 16:03:15 +1000

    fix test codes

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,9 +3,14 @@
 #'
 #' @return TRUE if test server is available.
 is_test_tw <- function() {
-    status <- request(httr::GET, "status")
-    if (httr::status_code(status) == "200") {
-        return(TRUE)
+    x <- try({
+        status <- request(httr::GET, "status")
+        if (httr::status_code(status) == "200") {
+            return(TRUE)
+        }
+    })
+    if (inherits(x, "try-error")) {
+        return(FALSE)
     }
     return(FALSE)
 }
