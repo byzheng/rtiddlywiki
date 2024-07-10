567832e8e0b4b82c983af3b90c0e90af86ad2cc1
Bangyou Zheng <bangyou.zheng@csiro.au>
2024-07-10 10:04:38 +1000

    Update test

--- R/options.R
+++ R/options.R
@@ -19,7 +19,9 @@ TW_OPTIONS <- settings::options_manager(
 #'
 #' @section Supported options:
 #' The following options are supported
-#'  host: host of tiddlywiki
+#'  host: Host of tiddlywiki
+#'  recipe: Recipes are named lists of bags, ordered from lowest priority to highest
+#'  bag: Bags have access controls that determines which users can read or write to them
 #'
 #' @return the default and modified options.
 #' @export
--- tests/testthat/test-tiddler.R
+++ tests/testthat/test-tiddler.R
@@ -5,7 +5,7 @@ test_that("tiddler", {
     expect_no_error(put_tiddler("test1", "This is a test tiddler",
                                 type = "text/x-tiddlywiki",
                                 tags = c("TAG 1", "TAG2"),
-                                fields = c("f1" = "f1", "f2" = "f 2")))
+                                fields = list("f1" = "f1", "f2" = "f 2")))
     new_tiddler <- get_tiddler("test1")
     expect_equal(new_tiddler$title, "test1")
     expect_equal(new_tiddler$text, "This is a test tiddler")
@@ -21,11 +21,11 @@ test_that("tiddler", {
     fields = c("f2" = "f 2 again")
     expect_no_error(put_tiddler(title = "test1",
                                 tags = c("TAG 3", "TAG2"),
-                                fields = c("f2" = "f 2 again", "f3" = "f 3")))
+                                fields = list("f2" = "f 2 again", "f3" = "f 3")))
     new_tiddler <- get_tiddler("test1")
     expect_equal(new_tiddler$title, "test1")
     expect_equal(new_tiddler$text, "This is a test tiddler")
-    expect_equal(new_tiddler$type, "text/x-tiddlywiki")
+    expect_equal(new_tiddler$type, "text/vnd.tiddlywiki")
     expect_equal(new_tiddler$tags, c("TAG 3", "TAG2", "TAG 1"))
     expect_equal(new_tiddler$fields$f1, "f1")
     expect_equal(new_tiddler$fields$f2, "f 2 again")
