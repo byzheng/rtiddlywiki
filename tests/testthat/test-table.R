40126ae341cf81f54c1c9ea866ea0b8ead794f4f
Bangyou Zheng <bangyou.zheng@csiro.au>
2025-02-05 09:11:10 +1000

    Add a new function read tablr

---
+++ tests/testthat/test-table.R
@@ -0,0 +1,26 @@
+
+# Define test cases
+test_that("read_table correctly parses a table with a header", {
+    table <- "|!Col1 |!Col2 |\n|A |B |\n|C |D |"
+    df <- read_table(table, header = TRUE)
+
+    expect_equal(colnames(df), c("Col1", "Col2"))
+    expect_equal(nrow(df), 2)
+    expect_equal(df[1, 1], c("A"))
+    expect_equal(df[2, 1], c("C"))
+})
+
+test_that("read_table correctly parses a table without a header", {
+    table <- "|A |B |\n|C |D |"
+    df <- read_table(table, header = FALSE)
+
+    expect_equal(nrow(df), 2)
+    expect_equal(ncol(df), 2)
+    expect_equal(df[1, 1], c("A"))
+    expect_equal(df[2, 1], c("C"))
+})
+
+test_that("read_table handles empty input", {
+    table <- ""
+    expect_error(df <- read_table(table, header = TRUE))
+})
