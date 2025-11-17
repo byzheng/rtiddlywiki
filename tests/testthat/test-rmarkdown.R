52d6748afb2a431126053787ab6bc9580de623c5
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-11-18 09:40:12 +1000

    Update format

--- tests/testthat/test-rmarkdown.R
+++ tests/testthat/test-rmarkdown.R
@@ -13,15 +13,19 @@ test_that("Get title", {
     expect_equal(.get_title("title: test"), "test")
     expect_equal(.get_title("title: \"test\""), "test")
     expect_equal(.get_title("title: \"this is a test\""), "this is a test")
-    expect_equal(.get_title("title: \"this is a \"test\"\""),
-                 "this is a \"test\"")
+    expect_equal(
+        .get_title("title: \"this is a \"test\"\""),
+        "this is a \"test\""
+    )
 })
 
 
 test_that("pretty link", {
-    text <- c("See this \\[\\[This is a test\\]\\], link again",
-              "this is a new line",
-              "this line has \\[\\[link one\\]\\] and \\[\\[label|link two\\]\\]")
+    text <- c(
+        "See this \\[\\[This is a test\\]\\], link again",
+        "this is a new line",
+        "this line has \\[\\[link one\\]\\] and \\[\\[label|link two\\]\\]"
+    )
     text2 <- .pretty_link(text)
     expect_equal(text2[1], "See this [[This is a test]], link again")
     expect_equal(text2[2], "this is a new line")
@@ -30,10 +34,12 @@ test_that("pretty link", {
 
 
 test_that("markdown link", {
-    text <- c("See this [This is a link](), link again",
-              "this is a new line",
-              "this line has [link one](#link 1) and [link two](#link2)",
-              "[a mornal link](https://example.org)")
+    text <- c(
+        "See this [This is a link](), link again",
+        "this is a new line",
+        "this line has [link one](#link 1) and [link two](#link2)",
+        "[a mornal link](https://example.org)"
+    )
     text2 <- .markdown_link(text)
     expect_equal(text2[1], "See this [This is a link](), link again")
     expect_equal(text2[2], "this is a new line")
@@ -43,8 +49,6 @@ test_that("markdown link", {
 
 
 test_that("rmarkdown", {
-
-
     skip_on_cran()
     skip_if_not_pandoc <- function() {
         if (!rmarkdown::pandoc_available()) {
@@ -62,8 +66,9 @@ test_that("rmarkdown", {
             temp_output_file <- paste0(tools::file_path_sans_ext(output_file), ".json")
         }
         rmarkdown::render(temp_input_file,
-                          output_file = output_file,
-                          quiet = TRUE)
+            output_file = output_file,
+            quiet = TRUE
+        )
         r <- jsonlite::read_json(temp_output_file)
         file.remove(temp_output_file, temp_input_file)
         r
@@ -72,177 +77,199 @@ test_that("rmarkdown", {
     skip_if_not_pandoc()
     # Test output_file
     output_file <- tempfile()
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "---", "",
-                        "# Section 1",
-                        "This is a test"), output_file = output_file)
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "---", "",
+        "# Section 1",
+        "This is a test"
+    ), output_file = output_file)
 
 
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "---", "",
-                        "# Section 1",
-                        "This is a test"))
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "---", "",
+        "# Section 1",
+        "This is a test"
+    ))
     expect_equal(rmd$tags, "[[tag1]] [[tag 2]]")
     expect_equal(rmd$title, "test")
 
-    rmd <- render_rmd(c("---", "title: test",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "---", "",
-                        "# Section 1",
-                        "This is a test"))
+    rmd <- render_rmd(c(
+        "---", "title: test",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "---", "",
+        "# Section 1",
+        "This is a test"
+    ))
     expect_equal(rmd$tags, "[[tag1]] [[tag 2]]")
     expect_equal(rmd$title, "test")
 
-    rmd <- render_rmd(c("---", "title: test",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    use_bookdown: true",
-                        "    number_sections: false",
-                        "---", "",
-                        "# Section 1",
-                        "This is a test"))
+    rmd <- render_rmd(c(
+        "---", "title: test",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    use_bookdown: true",
+        "    number_sections: false",
+        "---", "",
+        "# Section 1",
+        "This is a test"
+    ))
     expect_equal(rmd$tags, "[[tag1]] [[tag 2]]")
     expect_equal(rmd$title, "test")
     expect_equal(rmd$type, "text/x-markdown")
 
 
     # Test renderwikitext
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "---", "",
-                        "[[This is a test]]"))
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "---", "",
+        "[[This is a test]]"
+    ))
     expect_equal(rmd$text, "[[This is a test]]")
 
 
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "---", "",
-                        "{{This is a test}}"))
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "---", "",
+        "{{This is a test}}"
+    ))
     expect_equal(rmd$text, "{{This is a test}}")
 
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "---", "",
-                        "test [md link](#tiddler 1) and [link](#tiddler2)"))
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "---", "",
+        "test [md link](#tiddler 1) and [link](#tiddler2)"
+    ))
     expect_equal(rmd$text, "test [md link](#tiddler%201) and [link](#tiddler2)")
 
     # Test fields
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "{{This is a test}}"))
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "{{This is a test}}"
+    ))
     expect_equal(rmd$fields$field1, "V1")
     expect_equal(rmd$fields$`field 2`, "Value 2")
 
     # Test graph
 
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "```{r static-images-1}",
-                        "",
-                        "library(ggplot2)",
-                        "cars |> ",
-                        "    ggplot() +",
-                        "    geom_point(aes(speed, dist))",
-                        "```"))
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "```{r static-images-1}",
+        "",
+        "library(ggplot2)",
+        "cars |> ",
+        "    ggplot() +",
+        "    geom_point(aes(speed, dist))",
+        "```"
+    ))
     expect_equal(grepl("data:image", rmd$text), TRUE)
 
 
     # Test macro
 
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "",
-                        "<<macro>>",
-                        '<<macro p1:"P">>',
-                        "<<macro ",
-                        ">>"
-                        ))
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "",
+        "<<macro>>",
+        '<<macro p1:"P">>',
+        "<<macro ",
+        ">>"
+    ))
     expect_equal(grepl("<<macro>>", rmd$text), TRUE)
 
 
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "",
-                        '<<alert primary """',
-                        'This is a test',
-                        '""">>'
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "",
+        '<<alert primary """',
+        "This is a test",
+        '""">>'
     ))
     expect_equal(grepl('"""', rmd$text), TRUE)
     expect_equal(grepl("<<alert", rmd$text), TRUE)
 
     # Test variant: gfm
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    variant: gfm",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "",
-                        '<<alert primary """',
-                        'This is a test',
-                        '""">>'
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    variant: gfm",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "",
+        '<<alert primary """',
+        "This is a test",
+        '""">>'
     ))
     expect_equal(grepl('"""', rmd$text), TRUE)
     expect_equal(grepl("<<alert", rmd$text), TRUE)
 
     # Test widget
-    rmd <- render_rmd(c("---", "title: \"test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    variant: gfm",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "",
-                        '| <$widget p="C" /> |',
-                        '| <$widget p="C" /> |',
-                        '| <$widget p="C" /> |'
+    rmd <- render_rmd(c(
+        "---", "title: \"test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    variant: gfm",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "",
+        '| <$widget p="C" /> |',
+        '| <$widget p="C" /> |',
+        '| <$widget p="C" /> |'
     ))
-    expect_equal(grepl('<\\$widget', rmd$text), TRUE)
+    expect_equal(grepl("<\\$widget", rmd$text), TRUE)
 
 
 
@@ -250,38 +277,42 @@ test_that("rmarkdown", {
     # Test Tddlywiki
     skip_if(!is_test_tw())
 
-    rmd <- render_rmd(c("---", "title: \"rmarkdown test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    remote: false",
-                        "---", "",
-                        "```{r static-images-1}",
-                        "",
-                        "library(ggplot2)",
-                        "cars |> ",
-                        "    ggplot() +",
-                        "    geom_point(aes(speed, dist))",
-                        "```"))
+    rmd <- render_rmd(c(
+        "---", "title: \"rmarkdown test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    remote: false",
+        "---", "",
+        "```{r static-images-1}",
+        "",
+        "library(ggplot2)",
+        "cars |> ",
+        "    ggplot() +",
+        "    geom_point(aes(speed, dist))",
+        "```"
+    ))
     tiddler <- get_tiddler("rmarkdown test")
     expect_null(tiddler)
 
-    rmd <- render_rmd(c("---", "title: \"rmarkdown test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    remote: true",
-                        sprintf('    host: "%s"', tw_options("host")),
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "```{r static-images-1}",
-                        "",
-                        "library(ggplot2)",
-                        "cars |> ",
-                        "    ggplot() +",
-                        "    geom_point(aes(speed, dist))",
-                        "```"))
+    rmd <- render_rmd(c(
+        "---", "title: \"rmarkdown test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    remote: true",
+        sprintf('    host: "%s"', tw_options("host")),
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "```{r static-images-1}",
+        "",
+        "library(ggplot2)",
+        "cars |> ",
+        "    ggplot() +",
+        "    geom_point(aes(speed, dist))",
+        "```"
+    ))
     tiddler <- get_tiddler("rmarkdown test")
     expect_equal(tiddler$fields$field1, "V1")
     expect_equal(tiddler$fields$`field 2`, "Value 2")
@@ -291,22 +322,24 @@ test_that("rmarkdown", {
     expect_no_error(delete_tiddler("rmarkdown test"))
 
     # Test output file
-    rmd <- render_rmd(c("---", "title: \"rmarkdown test\"",
-                        "output: ",
-                        "  tiddler_document:",
-                        "    remote: true",
-                        "    tags: [\"tag1\", \"tag 2\"]",
-                        "    fields:",
-                        "      \"field1\": \"V1\"",
-                        "      \"field 2\": \"Value 2\"",
-                        "---", "",
-                        "```{r static-images-1}",
-                        "",
-                        "library(ggplot2)",
-                        "cars |> ",
-                        "    ggplot() +",
-                        "    geom_point(aes(speed, dist))",
-                        "```"), output_file = output_file)
+    rmd <- render_rmd(c(
+        "---", "title: \"rmarkdown test\"",
+        "output: ",
+        "  tiddler_document:",
+        "    remote: true",
+        "    tags: [\"tag1\", \"tag 2\"]",
+        "    fields:",
+        "      \"field1\": \"V1\"",
+        "      \"field 2\": \"Value 2\"",
+        "---", "",
+        "```{r static-images-1}",
+        "",
+        "library(ggplot2)",
+        "cars |> ",
+        "    ggplot() +",
+        "    geom_point(aes(speed, dist))",
+        "```"
+    ), output_file = output_file)
     tiddler <- get_tiddler("rmarkdown test")
     expect_equal(tiddler$fields$field1, "V1")
     expect_equal(tiddler$fields$`field 2`, "Value 2")
@@ -315,4 +348,3 @@ test_that("rmarkdown", {
     expect_equal(tiddler$type, "text/x-markdown")
     expect_no_error(delete_tiddler("rmarkdown test"))
 })
-
